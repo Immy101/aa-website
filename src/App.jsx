80313ea12608a532a5532c9bfd8cb8858cac3d1b
mary <immaculatekampire4@gmail.com>
2026-08-10 16:09:49 +0400

    button added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
  
   return (
     <>
+
     {/*motion commands are called props aka properties */}
     {/**easeIn and easeOut are the standard for smooth motions */}
     {/*transition property controls the timing and the ease property controls the the tempo with which the animation flows */}
@@ -15,8 +16,31 @@ function App() {
       animate={{ opacity:1, x:0}} 
       transition={{ duration:0.8, ease:"easeInOut" }}
       >
-        <h1>Hello World</h1>
+        <h1>AppleFrame</h1>
+      </motion.div> 
+      <motion.div
+      initial={{ opacity:0, x:-50}}
+      animate={{ opacity:1, x:0}}
+      transition={{ duration:0.8, ease:"easeInOut"}}
+      >
+        <p>In all of the famous cities of ruin, we have had a thousand burn and fall for this collection created by the one and only
+          Donatello Versace and none can curate such famous looks with the carefree essence he so desperately wanted. Not everyone can create such an essence.
+          However, forgiving as he is, he made sure to release the collection for the world to see and share in his spoil, thus creating new collections.
+
+        </p>
       </motion.div>
+      {/**button grows bigger with scale */}
+      {/*transition has type which gives the buttons a kind of effect*/}
+      {/** */}
+      <motion.button
+      whileHover={{ scale:1.05, y:-5 }} 
+      whileTap={{ scale:1, y:5}}
+      transition={{ type:"spring", stiffness:100}}
+      >
+        Let's start
+      </motion.button>
+    
+    {/**creating a hover motion */}
     </>
   )
 }
